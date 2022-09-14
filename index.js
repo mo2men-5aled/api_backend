63bd6df0c8fd1f77200a8b67fdd255dca46c1a47
momen <momenk208@gmail.com>
2022-09-14 16:14:20 +0200

    use url module to split the query into readable parts

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var http = require("http");
-
+var url = require("url");
 //create a server object:
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(req.url); //write a response to the client with the url that comes after the domain name
-    res.end(); //end the response
+    var q = url.parse(req.url, true).query; //use url module to split the query into readable parts
+    var txt = q.year + " " + q.month + " " + q.day; //their values been given after the domain name http://localhost:8000/?year=2000&month=jun&day=13
+    res.end(txt); //end the response
   })
-  .listen(8081); //the server object listens on port 8080
+  .listen(8080); //the server object listens on port 8080
 
 //The function passed into the http.createServer() method,
 //will be executed when someone tries to access the computer on port 8080.
