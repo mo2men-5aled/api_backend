5669b94fe20424b1d0bf4fc77d0df8ce0d8b7fcc
momen <momenk208@gmail.com>
2022-09-14 22:28:53 +0200

    install mysql & install mysql module & make connection

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-var nodemailer = require("nodemailer");
+var mysql = require("mysql");
 
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "khaledmuhammedmomen@gmail.com",
-    pass: "mo2men@2468",
-  },
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "momen",
+  password: "Mo2men@2468",
 });
 
-var mailOptions = {
-  from: "khaledmuhammedmomen@gmail.com",
-  to: "momenk208@gmail.com",
-  subject: "Sending Email using Node.js",
-  text: "That was easy!",
-};
-
-transporter.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Email sent: " + info.response);
-  }
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
