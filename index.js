7a87f90b2899873bdd5c5fe6e57aced13d3dd406
momen <momenk208@gmail.com>
2022-09-14 16:03:37 +0200

    write a response to the client with the url that comes after the domain name

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
-    res.write("Hello World!"); //write a response to the client
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(req.url); //write a response to the client with the url that comes after the domain name
     res.end(); //end the response
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(8081); //the server object listens on port 8080
 
 //The function passed into the http.createServer() method,
 //will be executed when someone tries to access the computer on port 8080.
