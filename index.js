f3807fcf27b0449b91d5b5310c606da278a33099
momen <momenk208@gmail.com>
2022-09-14 17:25:11 +0200

    append file()

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ var http = require("http");
 
 //include the built-in filesystem(fs) module
 var fs = require("fs");
-
-//create a server object:
-http
-  .createServer(function (req, res) {
-    fs.readFile("./index.html", function (err, data) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data); //data returned from the HTML file(index.html)
-      return res.end(); //end the response
-    });
-  })
-  .listen(8080); //the server object listens on port 8080
-
+fs.appendFile("test.txt", "hello there", function (err) {
+  if (err) throw err;
+  console.log("saved");
+});
 //The function passed into the http.createServer() method,
 //will be executed when someone tries to access the computer on port 8080.
