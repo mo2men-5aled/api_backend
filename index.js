f6b8fece42ae9084c740289edda29caae85fb4e6
momen <momenk208@gmail.com>
2022-09-14 15:53:26 +0200

    create a demo http server

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello World!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
+
+//The function passed into the http.createServer() method,
+//will be executed when someone tries to access the computer on port 8080.
