250ae44dd9db1f5c2f5b3bcf418d9168578ea58e
momen <momenk208@gmail.com>
2022-09-15 17:02:12 +0200

    SELECT & filter (WHERE)

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ var con = mysql.createConnection({
 
 con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
-  var sql =
-    "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table altered");
-  });
+  con.query(
+    "SELECT * FROM customers WHERE address = 'Park Lane 38'",
+    function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
 });
