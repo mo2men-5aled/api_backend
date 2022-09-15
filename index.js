59611b2c4d454d9bb707e2eaf4dfc948254e16ab
momen <momenk208@gmail.com>
2022-09-15 18:57:41 +0200

    install mongoDB & create a new database

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-var mysql = require("mysql");
+var MongoClient = require("mongodb").MongoClient;
+const url =
+  "mongodb+srv://momen:mo2men@cluster0.cnrgfls.mongodb.net/?retryWrites=true&w=majority";
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Mo2men@2468",
-  database: "mydb",
-});
-
-con.connect(function (err) {
-  var adr = "Mountain 21";
-
-  //var sql = `SELECT * FROM customers WHERE address = "${adr}"`;
-
-  //also you can escape using the "?" in the query and pass the value to the query function
-  // var sql = "SELECT * FROM customers WHERE address = ?";
-  // con.query(sql, [adr], function (err, result) {
-
-  var sql = "SELECT * FROM customers WHERE address = " + mysql.escape(adr);
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
+  console.log("Database created!");
+  db.close();
 });
