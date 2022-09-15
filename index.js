33efae457b3310e1308e58e294252cddb8f9c0ba
momen <momenk208@gmail.com>
2022-09-15 10:28:28 +0200

    create a database

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var mysql = require("mysql");
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "momen",
+  user: "root",
   password: "Mo2men@2468",
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 });
