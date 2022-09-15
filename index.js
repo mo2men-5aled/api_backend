9de6d5d2a7497a014f6f651340ad9bdf659bc25e
momen <momenk208@gmail.com>
2022-09-15 10:46:38 +0200

    edit table and add new column for key

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ var con = mysql.createConnection({
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  var sql =
+    "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log("Table altered");
   });
 });
