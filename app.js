e702074966412da34baee12f949e619b7a719c40
momen <momenk208@gmail.com>
2022-09-16 16:54:25 +0200

    os module methodes

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var os = require("os");
-
-var user = os.userInfo();
+const os = require("os");
+const user = os.uptime();
 console.log(user);
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  total_memory: os.totalmem(),
+  free_memory: os.freemem(),
+};
+console.log(currentOS);
