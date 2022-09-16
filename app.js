7926b4b7407ed44dcfceac8af87677b5bcf97458
momen <momenk208@gmail.com>
2022-09-16 23:42:08 +0200

    file system modules (Sync)

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const os = require("os");
+const { readFileSync, writeFileSync } = require("fs");
 
-const user = os.arch(); //x64
-console.log(user);
+const first = readFileSync("./first.txt", "utf8");
+const second = readFileSync("./second.txt", "utf8");
 
-const user1 = os.cpus(); //Returns an array containing information about the computer's CPUs
-console.log(user1);
-// {
-//   model: 'Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz',
-//   speed: 2000,
-//   times: { user: 2574950, nice: 8980, sys: 733760, idle: 51216220, irq: 0 }
-// }
-
-const user2 = os.endianness();
-console.log(user2); //LE
-
-//endianness:the term used to describe the byte order of data
-
-const user3 = os.platform(); //os Name
-console.log(user3);
+writeFileSync("./first.txt", "i'm the added text", { flag: "a" });
+console.log(first, second);
