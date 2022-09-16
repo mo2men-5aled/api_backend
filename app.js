8adcc19e84c0e877c6f1491fb741b1304c6b0e8d
momen <momenk208@gmail.com>
2022-09-16 23:44:07 +0200

    ////

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const first = readFileSync("./first.txt", "utf8");
 const second = readFileSync("./second.txt", "utf8");
 
 writeFileSync("./first.txt", "i'm the added text", { flag: "a" });
+//this {flag:"a"} means "append" that makes the file system modules add a new text to the file not to override it
 console.log(first, second);
