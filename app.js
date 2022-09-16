a4332ca5f6c1d80d34c84c080ad98829a6c2f657
momen <momenk208@gmail.com>
2022-09-16 18:39:04 +0200

    Other os methodes

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const os = require("os");
-const user = os.uptime();
+
+const user = os.arch(); //x64
 console.log(user);
-const currentOS = {
-  name: os.type(),
-  release: os.release(),
-  total_memory: os.totalmem(),
-  free_memory: os.freemem(),
-};
-console.log(currentOS);
+
+const user1 = os.cpus(); //Returns an array containing information about the computer's CPUs
+console.log(user1);
+// {
+//   model: 'Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz',
+//   speed: 2000,
+//   times: { user: 2574950, nice: 8980, sys: 733760, idle: 51216220, irq: 0 }
+// }
+
+const user2 = os.endianness();
+console.log(user2); //LE
+
+//endianness:the term used to describe the byte order of data
+
+const user3 = os.platform(); //os Name
+console.log(user3);
