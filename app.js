3416ac69ba5288fb65d2e11fff92ef7d45fc97b1
momen <momenk208@gmail.com>
2022-09-17 21:16:04 +0200

    added start to scripts to run nodemon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
-    res.write("Hello World :)");
+    res.write("Hello Worllllllllllld :)");
     res.end();
   } else if (req.url == "/about") {
     res.write("this is the about page");
