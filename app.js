7c24583b443a9800000c4b5662e62112c0106fdb
momen <momenk208@gmail.com>
2022-09-17 14:45:42 +0200

    fix code

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
     res.write("Hello World :)");
-  }
-  if (req.url == "/about") {
+    res.end();
+  } else if (req.url == "/about") {
     res.write("this is the about page");
-  }
-  return res.write(`<div>
+    res.end();
+  } else {
+    res.end(`<div>
       <div>oops</div>
       <a href="/">back home</a>
     </div>`);
+  }
 });
 
 server.listen(5050);
