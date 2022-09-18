7fe3459a6e579892a31e802be336c2ab69e6b142
momen <momenk208@gmail.com>
2022-09-18 22:47:30 +0200

    events module

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-const { readFile, writeFile } = require("fs").promises;
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
 
-const start = async () => {
-  try {
-    const file_1_respones = await readFile("./test.txt", "utf8");
-    const file_2_respones = await readFile("./test2.txt", "utf8");
-    await writeFile(
-      "./the_result.txt",
-      `i'm the first: ${file_1_respones}, and i'm the second one : ${file_2_respones}`,
-      { flag: "a" }
-    );
-    console.log(file_1_respones, file_2_respones);
-  } catch (err) {
-    console.log(err);
-  }
-};
+eventEmitter.on("response", () => {
+  console.log("i recieved the event");
+});
 
-start();
-
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
+eventEmitter.emit("response");
