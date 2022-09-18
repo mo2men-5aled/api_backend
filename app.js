17b9bdc0fa62362aa3b95c49ad240539620f4652
momen <momenk208@gmail.com>
2022-09-18 22:26:31 +0200

    read and write in files (node native option)

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
+const util = require("util");
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const respones = await getText("./test.txt");
-    console.log(respones);
+    const file_1_respones = await readFilePromise("./test.txt", "utf8");
+    const file_2_respones = await readFilePromise("./test2.txt", "utf8");
+    await writeFilePromise(
+      "./the_result.txt",
+      `i'm the first: ${file_1_respones}, and i'm the second one : ${file_2_respones}`
+    );
+    console.log(file_1_respones, file_2_respones);
   } catch (err) {
     console.log(err);
   }
 };
 
 start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
