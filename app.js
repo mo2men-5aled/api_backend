d3adcb86f048ac3bda769b74e37c176e32288d23
momen <momenk208@gmail.com>
2022-09-19 22:58:01 +0200

    return data to the page depending on the query

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const app = require("express")();
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
+});
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("HEllo World");
+});
+
+app.get("/api/v1/query", (req, res) => {
+  //console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  res.status(200).json(sortedProducts);
+});
 
 app.listen(5000, () => {
   console.log("app is listening on port 5000");
