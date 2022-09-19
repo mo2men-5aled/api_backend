96a19f60edce693aa6ec0bc478f2a6fcc362ec6c
momen <momenk208@gmail.com>
2022-09-19 18:53:01 +0200

    express.js app

--- app.js
+++ app.js
@@ -1,35 +1,7 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const app = require("express")();
+// const express = require("express");
+// const app = express();
 
-// for printing data in the server from html file
-// read file using fs module
-// define what type of data you will return in the page in the writeHead() methode {"content-type":"text/html"}
-// print it in the page using response.write(file)
-
-const portfolio = readFileSync("./index.html");
-const portfolio_css = readFileSync("./portfolio.css");
-const earth = readFileSync("./images/18.png");
-const earth2 = readFileSync("./images/19.png");
-
-const server = http.createServer((request, response) => {
-  console.log(request.url);
-  if (request.url === "/") {
-    response.writeHead(200, { "content-type": "text/html" });
-    response.write(portfolio);
-    response.end();
-  } else if (request.url === "/portfolio.css") {
-    response.writeHead(200, { "content-type": "text/css" });
-    response.write(portfolio_css);
-    response.end();
-  } else if (request.url === "/images/18.png") {
-    response.writeHead(200, { "content-type": "image/png" });
-    response.write(earth);
-    response.end();
-  } else if (request.url === "/images/19.png") {
-    response.writeHead(200, { "content-type": "image/png" });
-    response.write(earth2);
-    response.end();
-  }
+app.listen(5000, () => {
+  console.log("app is listening on port 5000");
 });
-
-server.listen(3000);
