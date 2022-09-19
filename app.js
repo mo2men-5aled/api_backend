72d7e076e40b1213d8c4ad4b2d6ca13b028ab660
momen <momenk208@gmail.com>
2022-09-19 23:37:06 +0200

    using middle ware (logger)

--- app.js
+++ app.js
@@ -1,34 +1,20 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1><a href='/api/products'>Products</a>");
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("Home Page");
 });
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("HEllo World");
-});
 
-app.get("/api/v1/query", (req, res) => {
-  //console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    res.status(200).send("No products match your search");
-  }
-  res.status(200).json(sortedProducts);
+app.get("/about", logger, (req, res) => {
+  res.send("About Page");
 });
 
 app.listen(5000, () => {
