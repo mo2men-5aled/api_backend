aedf0ae59a422be92eff9fbbd568b82b48509670
momen <momenk208@gmail.com>
2022-09-20 21:21:42 +0200

    another way for setting up routes

--- routes/people.js
+++ routes/people.js
@@ -8,14 +8,8 @@ const {
   deletePerson,
 } = require("../controller/people");
 
-router.get("/", getPeople);
-
-router.post("/", createPerson);
-
-router.post("/postman", createPersonPostman);
-
-router.put("/:id", updatePerson);
-
-router.delete("/:id", deletePerson);
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
