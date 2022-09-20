410593fd8fbbfe87dcd98935029a4be3d3d9a8fe
momen <momenk208@gmail.com>
2022-09-20 18:51:17 +0200

    testing api using postman

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ app.use(express.static("./methods-public"));
 //parsing from data
 app.use(express.urlencoded({ extended: false }));
 
+//parse Json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/people", (req, res) => {
   const { name } = req.body;
-
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value " });
   }
-  res.status(401).send("Please provide credintials");
+  res.status(201).send({ success: true, person: name });
 });
 
 app.listen(5000, () => {
