3d7ee17afe66f201bc01c33170c51354735c0dc7
momen <momenk208@gmail.com>
2022-09-21 17:59:34 +0200

    get person by params

--- src/Routes/person.js
+++ src/Routes/person.js
@@ -5,4 +5,9 @@ router.get("/person", (req, res) => {
   res.send("You have requested a person");
 });
 
-module.export = router;
+//http://localhost:3000/person/thomas
+router.get("/person/:name", (req, res) => {
+  res.send(`You have requested a person ${req.params.name}`); //thomas
+});
+
+module.exports = router;
