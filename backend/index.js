cda4aaae53e6b7de485ef18791a63e8cd4a15b1f
momen <momenk208@gmail.com>
2022-09-22 13:27:37 +0200

    connect to the database

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
+
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI)
+  .catch((err) => {
+    console.error(err.stack);
+  })
+  .then(async (client) => {
+    app.listen(port, () => {
+      console.log(`Listining on port ${port}`);
+    });
+  });
